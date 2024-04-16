37f4d8f8daefc6a49048b50ac22c3c31dc8b4d4d
Nikita Barskov <nikita.barskov@volur.no>
2024-04-16 12:49:39 +0200

    Add a test for fetching the required credentials from the environment

---
+++ tests/test_api/test_settings.py
@@ -0,0 +1,21 @@
+import pytest
+from volur.api.settings import VolurApiSettings
+
+
+@pytest.fixture()
+def expected_settings() -> VolurApiSettings:
+    return VolurApiSettings(
+        address="fake-address",
+        token="fake-token",
+    )
+
+
+def test_settings_should_obtain_configuration_from_environment(
+    monkeypatch: pytest.MonkeyPatch,
+    expected_settings: VolurApiSettings,
+) -> None:
+    with monkeypatch.context() as context:
+        context.setenv("VOLUR_API_ADDRESS", "fake-address")
+        context.setenv("VOLUR_API_TOKEN", "fake-token")
+        settings = VolurApiSettings()
+    assert expected_settings == settings
