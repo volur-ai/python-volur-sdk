c3036200ae022f609008b5b49767165dda3d1845
Nikita Barskov <nikita.barskov@volur.no>
2024-04-22 12:11:51 +0200

    Support `azure.functions.InputStream` as a an input.
    `azure.functions.InputStream` is a thin wrapper around
    `io.BufferedIOBase`
    
    See https://learn.microsoft.com/en-us/python/api/azure-functions/azure.functions.inputstream?view=azure-python
    and https://docs.python.org/3/library/io.html#io.BufferedIOBase
    
    Adding tests and updating the code base to support this.

--- tests/test_sources/csv/test_source.py
+++ tests/test_sources/csv/test_source.py
@@ -1,3 +1,4 @@
+import io
 from pathlib import Path
 
 import pytest
@@ -16,14 +17,25 @@ from volur.sdk.sources.csv import (
 
 
 @pytest.fixture()
-def csv_file(tmpdir: Path) -> str:
-    data = """id,plant,quantity,char_value
- 1,Plant1,100,1
- 2,Plant2,200,2"""
+def materials_csv_content() -> list[str]:
+    return [
+        "id,plant,quantity,char_value",
+        "1,Plant1,100,1",
+        "2,Plant2,200,2",
+    ]
+
+
+@pytest.fixture()
+def csv_file(
+    tmpdir: Path,
+    materials_csv_content: list[str],
+) -> str:
     # See https://stackoverflow.com/questions/40784950/pathlib-path-and-py-test-localpath
     path = Path(tmpdir / "test.csv")
     with open(path, "wb") as f:
-        f.write(data.encode("utf-8"))
+        for _ in materials_csv_content:
+            f.write(_.encode())
+            f.write("\n".encode())
     return str(path)
 
 
@@ -50,7 +62,7 @@ def asynchronous_csv_source(
 def expected_materials() -> list[material_pb2.Material]:
     return [
         material_pb2.Material(
-            material_id=" 1",
+            material_id="1",
             plant="Plant1",
             quantity=Quantity(value=QuantityValue(kilogram=100)),
             characteristics=[
@@ -61,7 +73,7 @@ def expected_materials() -> list[material_pb2.Material]:
             ],
         ),
         material_pb2.Material(
-            material_id=" 2",
+            material_id="2",
             plant="Plant2",
             quantity=Quantity(value=QuantityValue(kilogram=200)),
             characteristics=[
@@ -80,4 +92,40 @@ async def test_load_file(
     expected_materials: list[material_pb2.Material],
 ) -> None:
     actual_materials = [_ async for _ in asynchronous_csv_source]
+    assert actual_materials == expected_materials
+
+
+@pytest.fixture()
+def buffered_csv(materials_csv_content: list[str]) -> io.BufferedIOBase:
+    bio = io.BytesIO()
+    for _ in materials_csv_content:
+        bio.write(_.encode())
+        bio.write("\n".encode())
+    bio.seek(0)
+    return bio
+
+
+@pytest.fixture()
+def io_buffered_csv_source(buffered_csv: io.BufferedIOBase) -> MaterialsCSVFileSource:
+    return MaterialsCSVFileSource(
+        path=buffered_csv,
+        material_id_column=Column(column_name="id"),
+        plant_id_column=Column(column_name="plant"),
+        quantity_column=QuantityColumn(column_name="quantity", unit="kilogram"),
+        characteristics_columns=[
+            CharacteristicColumn(
+                column_name="char_value",
+                characteristic_name="char_value",
+                data_type="integer",
+            )
+        ],
+    )
+
+
+@pytest.mark.asyncio()
+async def test_load_from_io_buffered(
+    io_buffered_csv_source: MaterialsCSVFileSource,
+    expected_materials: list[material_pb2.Material],
+) -> None:
+    actual_materials = [_ async for _ in io_buffered_csv_source]
     assert actual_materials == expected_materials
